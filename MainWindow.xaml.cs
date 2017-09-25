8beea7eb56d93b6c117e96c6a761206f347fd637
Dijji <Dijji@outlook.com>
2017-09-25 21:08:14 +0100

    Initial code commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WatiN.Core;
+using Microsoft.Office.Interop.Word;
+using Find = WatiN.Core.Find;
+using System.Threading;
+using System.Xml.Serialization;
+
+namespace GetDiscussions
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Get_Click(object sender, RoutedEventArgs e)
+        {
+            status.Content = "Reading from CodePlex...";
+            var siteName = site.Text.Trim();
+            var toWord = (bool)checkWord.IsChecked;
+            var toXml = (bool)checkXml.IsChecked;
+
+            var task = StartSTATask<string>(() =>
+            {
+                return GetDiscussions(siteName, toWord, toXml);
+            })
+            .ContinueWith((t) =>
+            {
+                System.Windows.Application.Current.Dispatcher.Invoke(() =>
+                {
+                    if (t.Result == null)
+                        status.Content = "Success!";
+                    else
+                        status.Content = "Get failed with: " + t.Result;
+                });
+            });
+        }
+
+        private string GetDiscussions(string siteName, bool toWord, bool toXml)
+        {
+            try
+            {
+                List<Discussion> discs = new List<Discussion>();
+                using (var ie = new IE())
+                {
+                    var ds = GetDiscussionUrls(ie, String.Format("https://{0}.codeplex.com/", siteName));
+                    foreach (var url in ds)
+                    {
+                        discs.Add(GetDiscussion(ie, url));
+                    }
+                }
+
+                if (toWord)
+                {
+                    System.Windows.Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        status.Content = "Creating Word document…";
+                    });
+
+                    var word = new Microsoft.Office.Interop.Word.Application();
+                    object missing = System.Reflection.Missing.Value;
+                    var doc = word.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+                    var HtmlTempPath = Path.Combine(Path.GetTempPath(), $"{Path.GetRandomFileName()}.html");
+
+                    foreach (var disc in discs)
+                    {
+                        Paragraph para2 = doc.Content.Paragraphs.Add(ref missing);
+                        // Text must be set before Style for the style to be applied properly
+                        para2.Range.Text = disc.Title;
+                        para2.Range.set_Style(WdBuiltinStyle.wdStyleHeading2);
+                        para2.Range.InsertParagraphAfter();
+
+                        if (disc.Posts.Count > 0)
+                        {
+                            var table = doc.Tables.Add(para2.Range, disc.Posts.Count, 2, ref missing, ref missing);
+                            table.AllowAutoFit = true;
+                            table.Borders.Enable = 1;
+
+
+                            foreach (Row row in table.Rows)
+                            {
+                                var p = disc.Posts[row.Index - 1];
+                                row.Cells[1].Range.Text = p.From + "\n" + p.When;
+
+                                File.WriteAllText(HtmlTempPath, $"<html>{p.Html}</html>");
+                                row.Cells[2].Range.InsertFile(HtmlTempPath, ref missing, ref missing, ref missing, ref missing);
+                            }
+                        }
+                    }
+                    
+                    doc.SaveAs2(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), siteName + " Discussions.docx"));
+                    doc.Close();
+                    word.Quit();
+                    File.Delete(HtmlTempPath);
+                }
+
+                if (toXml)
+                {
+                    // Output XML
+                    var ds = new DiscussionSet { Discussions = discs };
+                    XmlSerializer x = new XmlSerializer(typeof(DiscussionSet));
+                    using (TextWriter writer = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), siteName + " Discussions.xml")))
+                    {
+                        x.Serialize(writer, ds);
+                    }
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        private List<string> GetDiscussionUrls(IE ie, string siteUrl)
+        {
+            List<string> discussions = new List<string>();
+            ie.GoTo(siteUrl);
+            ie.WaitForComplete();
+            
+            bool done = false;
+            ie.Link(Find.ById("discussionTab")).Click();
+            while (!done)
+            {
+                ie.WaitForComplete();
+                foreach (var div in ie.Divs)
+                {
+                    if (div.ClassName == "post_content")
+                    {
+                        discussions.Add(div.Link(Find.First()).Url);
+                    }
+                }
+
+                var pag = ie.List(Find.ById("discussion_pagination"));
+                var link = pag.Link(Find.ByText("Next"));
+                if (link.Exists)
+                    link.Click();
+                else
+                    done = true;
+            }
+
+            return discussions;
+        }
+
+        private Discussion GetDiscussion(IE ie, string url)
+        {
+            var disc = new Discussion();
+            ie.GoTo(url);
+            ie.WaitForComplete();
+
+            var hDiv = ie.Div(Find.ByClass("ViewThread"));
+            var h = hDiv.Elements.First();
+            disc.Title = h.InnerHtml.Replace("\n", "").Trim();
+
+            var tDiv = ie.Div(Find.ByClass("Posts"));
+            foreach(var tr in tDiv.TableRows)
+            {
+                if (tr.Id == "PostPanel")
+                {
+                    var p = new Post();
+                    var details = tr.TableCells[0];
+                    p.From = details.Div(Find.ByClass("UserName")).Elements.First().InnerHtml;
+                    p.When = details.Span(Find.ByClass("smartDate")).Title;
+                    var content = tr.TableCells[1];
+                    p.Html = content.InnerHtml; 
+                    disc.Posts.Add(p);
+                }
+            }
+
+            return disc;
+        }
+
+        public class DiscussionSet
+        {
+            public List<Discussion> Discussions { get; set; }
+        }
+
+        public class Discussion
+        {
+            [XmlAttribute]
+            public string Title { get; set; }
+            public List<Post> Posts { get; private set; } = new List<Post>();
+        }
+
+        public struct Post
+        {
+            [XmlAttribute]
+            public string From { get; set; }
+            [XmlAttribute]
+            public string When { get; set; }
+            public string Html { get; set; }
+        }
+
+        private static Task<T> StartSTATask<T>(Func<T> func)
+        {
+            var tcs = new TaskCompletionSource<T>();
+            Thread thread = new Thread(() =>
+            {
+                try
+                {
+                    tcs.SetResult(func());
+                }
+                catch (Exception e)
+                {
+                    tcs.SetException(e);
+                }
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            return tcs.Task;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CaptureCodePlexDiscussions
 Capture all the discussions for a CodePlex project
+
+CodePlex is shutting down on December 15th 2017. As of the current plan of record (see Brian Harry's blog post at https://blogs.msdn.microsoft.com/bharry/2017/03/31/shutting-down-codeplex/), all discussion threads will be discarded at this point, although sources and downloads will be preserved.
+
+There have been some complaints in the comments on the blog page referenced above about the loss of discussions. However, the last post on the subject was the following entry from Alex Mullans of Microsoft four months ago:
+
+@dahmaninator We’ll be starting that work in the next sprint (which starts on 6/19), so I’d expect it to be available ~3 weeks after that.
+
+However, absolutely nothing has been heard of this since. Being unprepared to lose the discussions from my own projects, I've written my own discussion capture program, provided here.
+
+This project provides a program that will download all the discussion content for a CodePlex project and save it as a Word document (for easy reading), or an XML file (for programmatic access), or both.
+
+The program screen scrapes the CodePlex site, using WatiN to drive Internet Explorer. It is not particularly quick, but it appears to be fairly thorough: I've included some sample results of captures from different projects.
